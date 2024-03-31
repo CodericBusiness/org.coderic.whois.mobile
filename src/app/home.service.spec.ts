509a4dece8ac58777e069104c43007cf39ee3c04
Neftali Yagua <neftaliyagua@fedoraproject.org>
2024-03-31 07:38:18 -0500

    Fix test

--- src/app/home.service.spec.ts
+++ src/app/home.service.spec.ts
@@ -1,16 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
 import { HomeService } from './home.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HomeService', () => {
   let service: HomeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(HomeService);
   });
 
-  it('should be created', () => {
+  it('should service exist', () => {
     expect(service).toBeTruthy();
   });
 });
--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -2,23 +2,49 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HomeService, QueryInterface } from '../home.service';
+import { HttpClient } from '@angular/common/http';
+import data from 'src/assets/disney.json';
 
 describe('HomePage', () => {
   let component: HomePage;
+  let service: HomeService;
   let fixture: ComponentFixture<HomePage>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        HttpClientTestingModule,
+        IonicModule.forRoot()
+      ],
     }).compileComponents();
 
+    service = TestBed.inject(HomeService);
+    httpTestingController = TestBed.inject(HttpTestingController);
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const q: QueryInterface={
+      type: 'domain',
+      name: 'disney.com'
+    }
+    service.searchWhois(q)
+    .subscribe(data => {
+      console.dir(data);
+      expect(data).toEqual(data);
+    })
+    const req = httpTestingController.expectOne('https://rdap.org/domain/disney.com');
+    
+    expect(req.request.method).toEqual('GET');
+    req.flush(data);
+
+    httpTestingController.verify();
   });
 });
